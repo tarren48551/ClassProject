ebdc37f46268ab0dc580977a542b7bec41e468db
tarren48551 <tarren48551@cccneb.edu>
2017-03-07 21:11:15 -0600

    Initial commit

---
+++ app/src/main/java/com/tarren/classproject/CreateAccount.java
@@ -0,0 +1,17 @@
+package com.tarren.classproject;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by Tarren Policky
+ */
+
+public class CreateAccount extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_createaccount);
+    }
+}
